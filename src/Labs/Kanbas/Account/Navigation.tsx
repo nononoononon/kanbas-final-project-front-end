8f6edefb18a1b474e7e83b7e8ae7f0da1f53b130
nonononoonononon <yuangli971@gmail.com>
2024-10-15 21:25:28 -0400

    refactor account nav bar

--- src/Labs/Kanbas/Account/Navigation.tsx
+++ src/Labs/Kanbas/Account/Navigation.tsx
@@ -1,14 +1,27 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+
 export default function AccountNavigation() {
+    const { pathname } = useLocation();
+    const links = [
+        { label: "Signin", path: "/Kanbas/Account/Signin" },
+        { label: "Signup", path: "/Kanbas/Account/Signup" },
+        { label: "Profile", path: "/Kanbas/Account/Profile" }
+    ];
+
     return (
         <div id="wd-account-navigation" className="wd list-group fs-5 rounded-0">
-            <Link to={`/Kanbas/Account/Signin`} id="wd-account-signin-link"
-                  className="list-group-item active border border-0">
-                Signin </Link>
-            <Link to={`/Kanbas/Account/Signup`} id="wd-account-signup-link"
-                  className="list-group-item text-danger border border-0"> Signup </Link>
-            <Link to={`/Kanbas/Account/Profile`} id="wd-account-profile-link"
-                  className="list-group-item text-danger border border-0"> Profile </Link>
+            {links.map((link) => (
+                <Link
+                    key={link.path}
+                    to={link.path}
+                    id={`wd-account-${link.label.toLowerCase()}-link`}
+                    className={`list-group-item border border-0 ${
+                        pathname.includes(link.label) ? "active text-black bg-white" : "text-danger"
+                    }`}
+                >
+                    {link.label}
+                </Link>
+            ))}
         </div>
     );
 }
