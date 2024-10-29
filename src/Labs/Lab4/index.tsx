840e08b1c365d034b6c88a780ff21959ec1bb3a8
nonononoonononon <yuangli971@gmail.com>
2024-10-29 00:31:53 -0400

    lab 4.2

--- src/Labs/Lab4/index.tsx
+++ src/Labs/Lab4/index.tsx
@@ -9,6 +9,7 @@ import DateStateVariable from "./DateStateVariable";
 import ObjectStateVariable from "./ObjectStateVariable";
 import ArrayStateVariable from "./ArrayStateVariable";
 import ParentStateComponent from "./ParentStateComponent";
+import ReduxExamples from "./ReduxExamples";
 export default function Lab4() {
     function sayHello() {
         alert("Hello");
@@ -27,6 +28,8 @@ export default function Lab4() {
             <ObjectStateVariable/>
             <ArrayStateVariable/>
             <ParentStateComponent/>
+            <ReduxExamples/>
+
         </div>
     );
 }
