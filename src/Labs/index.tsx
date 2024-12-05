c2f878de1112e8528ab4c374bfa382cc34d36b82
nonononoonononon <yuangli971@gmail.com>
2024-12-05 13:22:23 -0500

    delete my homework

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -1,28 +1,21 @@
-import Lab1 from "./Lab1";
 import { Route, Routes, Navigate } from "react-router";
 import TOC from "./TOC";
-import Lab2 from "./Lab2";
-import Lab3 from "./Lab3";
-import Lab4 from "./Lab4";
-import store from "./store";
-import { Provider } from "react-redux";
-import Lab5 from "./Lab5";
 export default function Labs() {
     return (
-        <Provider store={store}>
         <div className="container-fluid">
-            <h1>Labs</h1>
-            <h2>Name : Yuang Li</h2>
+            <hr/>
             <TOC/>
             <Routes>
-                <Route path="/" element={<Navigate to="Lab1"/>}/>
-                <Route path="Lab1" element={<Lab1/>}/>
-                <Route path="Lab2" element={<Lab2/>}/>
-                <Route path="Lab3/*" element={<Lab3/>}/>
-                <Route path="Lab4/*" element={<Lab4/>}/>
-                <Route path="Lab5/*" element={<Lab5/>}/>
+                <Route path="/" element={<Navigate to="Kanbas"/>}/>
             </Routes>
+            <hr/>
+            <h1>Group Project</h1>'
+            <hr/>
+            <h2>Team Members: </h2>
+            <h3>Yuang Li </h3>
+            <h3>Jiazuo Zhang </h3>
+            <h3>xxx </h3>
+            <h3>xxx </h3>
         </div>
-        </Provider>
     );
 }
